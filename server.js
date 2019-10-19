ed3800280681f888f03bbf425621289e7eae1aef
huaxiabuluo <huaxiabuluo@hotmail.com>
2019-10-19 12:05:52 +0800

    hooks

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config.js');
 const portal = config.devServer.https ? 'https' : 'http';
 const port   = config.devServer.port;
-const ip     = internalIP.v4.sync();
+const ip     = '0.0.0.0';
 
 for (let key in config.entry) {
     const arr = config.entry[key];
@@ -23,5 +23,5 @@ new WebpackDevServer(webpack(config), config.devServer).listen(port, ip, (err) =
     if (err) { console.log(err); }
     console.log('Listening at localhost:' + port);
     console.log('Opening your system browser...');
-    open(portal + '://' + (ip || '127.0.0.1') + ':' + port + '/static/index.html?debug=true');
+    open(portal + '://' + '127.0.0.1' + ':' + port + '/static/index.html?debug=true');
 });
--- src/view/components/todo-text-input.js
+++ src/view/components/todo-text-input.js
@@ -1,47 +1,81 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import classnames from 'classnames';
 
-export default class TodoTextInput extends React.Component {
-    state = {
-        text: this.props.text || ''
-    }
+export default function TodoTextInput(props) {
+  const inputRef = useRef();
 
-    handleSubmit = e => {
-        const text = e.target.value.trim()
-        if (e.which === 13) {
-            this.props.onSave(text)
-            if (this.props.newTodo) {
-                this.setState({ text: '' })
-            }
-        }
+  const handleSubmit = useCallback(e => {
+    const text = e.target.value.trim()
+    if (e.which === 13) {
+      props.onSave(text)
+      if (props.newTodo) {
+        inputRef.current.value = '';
+      }
     }
+  }, [inputRef, props]);
 
-    handleChange = e => {
-        this.setState({ text: e.target.value })
-    }
+  const handleBlur = useCallback(e => !props.newTodo && props.onSave(e.target.value), [props]);
 
-    handleBlur = e => {
-        if (!this.props.newTodo) {
-            this.props.onSave(e.target.value)
-        }
-    }
+  const lastClass = classnames({
+    edit: props.editing,
+    'new-todo': props.newTodo
+  });
 
-    render() {
-        const lastClass = classnames({
-            edit: this.props.editing,
-            'new-todo': this.props.newTodo
-        });
-        return (
-            <input 
-                className={lastClass}
-                type="text"
-                placeholder={this.props.placeholder}
-                autoFocus={true}
-                value={this.state.text}
-                onBlur={this.handleBlur}
-                onChange={this.handleChange}
-                onKeyDown={this.handleSubmit} 
-            />
-        );
-    }
+  return (
+    <input
+      type="text"
+      ref={inputRef}
+      defaultValue={props.text || ''}
+      className={lastClass}
+      placeholder={props.placeholder}
+      autoFocus={true}
+      onBlur={handleBlur}
+      onKeyDown={handleSubmit}
+    />
+  );
 }
+
+// export default class TodoTextInput extends React.Component {
+//     state = {
+//         text: this.props.text || ''
+//     }
+
+//     handleSubmit = e => {
+//         const text = e.target.value.trim()
+//         if (e.which === 13) {
+//             this.props.onSave(text)
+//             if (this.props.newTodo) {
+//                 this.setState({ text: '' })
+//             }
+//         }
+//     }
+
+//     handleChange = e => {
+//         this.setState({ text: e.target.value })
+//     }
+
+//     handleBlur = e => {
+//         if (!this.props.newTodo) {
+//             this.props.onSave(e.target.value)
+//         }
+//     }
+
+//     render() {
+//         const lastClass = classnames({
+//             edit: this.props.editing,
+//             'new-todo': this.props.newTodo
+//         });
+//         return (
+//             <input 
+//                 className={lastClass}
+//                 type="text"
+//                 placeholder={this.props.placeholder}
+//                 autoFocus={true}
+//                 value={this.state.text}
+//                 onBlur={this.handleBlur}
+//                 onChange={this.handleChange}
+//                 onKeyDown={this.handleSubmit} 
+//             />
+//         );
+//     }
+// }
